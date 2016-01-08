f92e713da8c71af0fb5197cc13a505636abe8dc5
tangyi <tangyi@taobao.com>
2016-01-08 22:07:46 +0800

    完成教程 part1
    1. 什么是event, signal
    2. 怎么订阅一个signal
    3. 怎么组合多个signal
    4. 怎么map, filter
    5. 怎么处理signal of signal问题
    6. 怎么将异步代码表示为signal
    7. 怎么做一些'副作用'
    一切答案都在 http://www.raywenderlich.com/62699/reactivecocoa-tutorial-pt1

---
+++ Podfile
@@ -0,0 +1,5 @@
+source "git@gitlab.alibaba-inc.com:alipods/specs-mirror.git"
+
+platform :ios, '7.0'
+ 
+pod 'ReactiveCocoa', '2.1.8'
--- RWReactivePlayground/RWViewController.m
+++ RWReactivePlayground/RWViewController.m
@@ -9,6 +9,8 @@
 #import "RWViewController.h"
 #import "RWDummySignInService.h"
 
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
 @interface RWViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
@@ -16,8 +18,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *signInButton;
 @property (weak, nonatomic) IBOutlet UILabel *signInFailureText;
 
-@property (nonatomic) BOOL passwordIsValid;
-@property (nonatomic) BOOL usernameIsValid;
+//@property (nonatomic) BOOL passwordIsValid;
+//@property (nonatomic) BOOL usernameIsValid;
 @property (strong, nonatomic) RWDummySignInService *signInService;
 
 @end
@@ -26,17 +28,90 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  
-  [self updateUIState];
+    // 学习:
+    // 1. 如何订阅一个signal
+    // 2. signal还能使用filter, map这样的pipeline,过滤，转化一个signal
+    //    map返回的值都是Objectiv-C Object, 内置类型(NSInteger，BOOL)会被box成Objectiv-C对象
+//    [[self.usernameTextField.rac_textSignal
+//     filter:^BOOL(NSString *value) {
+//         return value.length > 3;
+//     }]
+//     subscribeNext:^(id x) {
+//         NSLog(@"%@", x);
+//     }];
+//    
+//  [self updateUIState]; // 改变button，textfield的颜色，
   
   self.signInService = [RWDummySignInService new];
   
+// 用了reactive后，这个addTarget:action:forControlEvent也不用了
   // handle text changes for both text fields
-  [self.usernameTextField addTarget:self action:@selector(usernameTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
-  [self.passwordTextField addTarget:self action:@selector(passwordTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
-  
+//  [self.usernameTextField addTarget:self action:@selector(usernameTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
+//  [self.passwordTextField addTarget:self action:@selector(passwordTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
+//  
   // initially hide the failure message
   self.signInFailureText.hidden = YES;
+    
+    RACSignal *validUsernameSignal =
+    [self.usernameTextField.rac_textSignal
+     map:^id(NSString *text) {
+         return @([self isValidUsername:text]);
+     }];
+    
+    RACSignal *validPasswordSignal =
+    [self.passwordTextField.rac_textSignal
+     map:^id(NSString *text) {
+         return @([self isValidPassword:text]);
+     }];
+    // 添加动作代码, updateUIState里面的赋值可以删掉了
+    RAC(self.passwordTextField, backgroundColor) =
+    [validPasswordSignal
+     map:^id(NSNumber *passwordValid) {
+         return [passwordValid boolValue] ? [UIColor clearColor] : [UIColor yellowColor];
+     }];
+    
+    RAC(self.usernameTextField, backgroundColor) =
+    [validUsernameSignal
+     map:^id(NSNumber *passwordValid) {
+         return [passwordValid boolValue] ? [UIColor clearColor] : [UIColor yellowColor];
+     }];
+    // 拼接两个signal, 将signIn button给改写为reactive-style
+    // combineLatest:reduce
+    RACSignal *signUpActiveSignal =
+    [RACSignal combineLatest:@[validUsernameSignal, validPasswordSignal]
+                      reduce:^id(NSNumber *usernameValid, NSNumber *passwordValid) {
+                          return @([usernameValid boolValue] && [passwordValid boolValue]);
+                      }];
+    [signUpActiveSignal subscribeNext:^(NSNumber *signupActive) {
+        self.signInButton.enabled = [signupActive boolValue];
+    }];
+    
+    // 给signIn button添加动作
+//    [[self.signInButton
+//      rac_signalForControlEvents:UIControlEventTouchUpInside]
+//     subscribeNext:^(id x) {
+//         NSLog(@"button clicked");
+//     }];
+//    
+    [[[[self.signInButton
+       rac_signalForControlEvents:UIControlEventTouchUpInside]
+      doNext:^(id x) {
+          self.signInButton.enabled = NO;
+          self.signInFailureText.hidden = YES;
+      }]
+      // map:^id(id x) {
+      flattenMap:^id(id x) {
+          return [self signInSignal];
+      }]
+     subscribeNext:^(NSNumber *signedIn) {
+         NSLog(@"Sign in result: %@", signedIn); // 这里做是否登录成功的判断
+         BOOL success = [signedIn boolValue];
+         self.signInFailureText.hidden = success;
+         if (success) {
+             [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+         }
+
+     }];
 }
 
 - (BOOL)isValidUsername:(NSString *)username {
@@ -47,40 +122,57 @@
   return password.length > 3;
 }
 
-- (IBAction)signInButtonTouched:(id)sender {
-  // disable all UI controls
-  self.signInButton.enabled = NO;
-  self.signInFailureText.hidden = YES;
-  
-  // sign in
-  [self.signInService signInWithUsername:self.usernameTextField.text
-                            password:self.passwordTextField.text
-                            complete:^(BOOL success) {
-                              self.signInButton.enabled = YES;
-                              self.signInFailureText.hidden = success;
-                              if (success) {
-                                [self performSegueWithIdentifier:@"signInSuccess" sender:self];
-                              }
-                            }];
-}
+// 这个函数是从IB里连过来的
+//- (IBAction)signInButtonTouched:(id)sender {
+//  // disable all UI controls
+//  self.signInButton.enabled = NO;
+//  self.signInFailureText.hidden = YES;
+//  
+//  // sign in
+//  [self.signInService signInWithUsername:self.usernameTextField.text
+//                            password:self.passwordTextField.text
+//                            complete:^(BOOL success) {
+//                              self.signInButton.enabled = YES;
+//                              self.signInFailureText.hidden = success;
+//                              if (success) {
+//                                [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+//                              }
+//                            }];
+//}
+//
 
+// 用这个方法来实现登陆动作
+-(RACSignal *)signInSignal {
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [self.signInService
+         signInWithUsername:self.usernameTextField.text
+         password:self.passwordTextField.text
+         complete:^(BOOL success) {
+             [subscriber sendNext:@(success)];
+             [subscriber sendCompleted];
+         }];
+        return nil;
+    }];
+}
 
 // updates the enabled state and style of the text fields based on whether the current username
 // and password combo is valid
-- (void)updateUIState {
-  self.usernameTextField.backgroundColor = self.usernameIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-  self.passwordTextField.backgroundColor = self.passwordIsValid ? [UIColor clearColor] : [UIColor yellowColor];
-  self.signInButton.enabled = self.usernameIsValid && self.passwordIsValid;
-}
+//- (void)updateUIState {
+//   // 注释掉了
+//  // self.usernameTextField.backgroundColor = self.usernameIsValid ? [UIColor clearColor] : [UIColor yellowColor];
+//  // self.passwordTextField.backgroundColor = self.passwordIsValid ? [UIColor clearColor] : [UIColor yellowColor];
+//  // self.signInButton.enabled = self.usernameIsValid && self.passwordIsValid;
+//}
 
-- (void)usernameTextFieldChanged {
-  self.usernameIsValid = [self isValidUsername:self.usernameTextField.text];
-  [self updateUIState];
-}
-
-- (void)passwordTextFieldChanged {
-  self.passwordIsValid = [self isValidPassword:self.passwordTextField.text];
-  [self updateUIState];
-}
+// reactive后，不用了
+//- (void)usernameTextFieldChanged {
+//  self.usernameIsValid = [self isValidUsername:self.usernameTextField.text];
+//  [self updateUIState];
+//}
+//
+//- (void)passwordTextFieldChanged {
+//  self.passwordIsValid = [self isValidPassword:self.passwordTextField.text];
+//  [self updateUIState];
+//}
 
 @end
